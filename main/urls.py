6ba58a0b867b0b463472f094c9d73660bc22e685
skiff <skiff.udev@gmail.com>
2019-12-12 19:59:25 +0200

    Add page for changing user password

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, BBLogoutView, profile, ChangeUserInfoView
+from .views import (index,
+                    other_page,
+                    BBLoginView,
+                    BBLogoutView,
+                    profile,
+                    ChangeUserInfoView,
+                    BBPasswordChangeView)
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/password/change', BBPasswordChangeView.as_view(), name='password_change'),
     path('accounts/profile/change', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
